831d2c6cdc7f2f4add102539b2db0c96d867378a
KirMac <komarova1610@yandex.ru>
2022-11-07 15:10:31 +0300

    add replay button and fix everything besides score

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const controls = document.querySelector('.controls');
 let changeColor;
 let totalBubbles = 0;
 let hits = 0;
+let timerExpire;
 
 ///////////////////////////////////////////////////////////
 // Functions
@@ -69,29 +70,53 @@ controls.addEventListener('mouseout', (e) => {
   }
 });
 
-controls.addEventListener('click', (e) => {
+function play(e) {
+  playgrounds.innerHTML = '';
+  document.querySelector('.results').classList.add('hidden');
+  document.querySelector('.game').classList.remove('hidden');
+  hits = 0;
+  totalBubbles = 0;
   let time = 30;
   timer.textContent = `00:${time}`;
-  if (e.target.classList.contains('start')) {
+  if (
+    e.target.classList.contains('start') ||
+    e.target.classList.contains('retry')
+  ) {
     changeColor = setInterval(function () {
       spawnCircle();
+    }, 500);
+    timerExpire = setInterval(function () {
       time = (--time).toString().padStart(2, '0');
       timer.textContent = `00:${time}`;
       if (time === '00') {
         clearInterval(changeColor);
+        clearInterval(timerExpire);
         document.querySelector('.game').classList.add('hidden');
         document.querySelector('.results').classList.remove('hidden');
         document.querySelector('.score').textContent = hits;
         document.querySelector('.total').textContent = totalBubbles;
+        const retry = document.querySelector('.retry');
+        retry.addEventListener('mouseover', (e) => {
+          e.target.classList.add('hovered');
+        });
+        retry.addEventListener('mouseout', (e) => {
+          e.target.classList.remove('hovered');
+        });
+        retry.addEventListener('click', (e) => play(e));
       }
-    }, 500);
+    }, 1000);
   }
-});
+}
+
+controls.addEventListener('click', (e) => play(e));
+
+// Differentiate timer and bubbles 1000 500 250
 
 controls.addEventListener('click', (e) => {
   if (e.target.classList.contains('stop')) {
     clearInterval(changeColor);
-    alert('Можете выдержать 30 секунд?');
+    clearInterval(timerExpire);
+    playgrounds.style.backgroundColor = 'blue';
   }
 });
 
